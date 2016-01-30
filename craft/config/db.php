ea04134d355bda7e61f7b1de21a3e4fca7cb8fd7
the incredible hulch <jhulcher@gmail.com>
2016-01-29 20:28:41 -0800

    Test 5

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,21 +7,29 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
+// return array(
+//
+// 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+// 	'server' => '127.0.0.1',
+//
+// 	// The name of the database to select.
+// 	'database' => 'para',
+//
+// 	// The database username to connect with.
+// 	'user' => 'root',
+//
+// 	// The database password to connect with.
+// 	'password' => '',
+//
+// 	// The prefix to use when naming tables. This can be no more than 5 characters.
+// 	'tablePrefix' => 'craft',
+//
+// );
+$url=parse_url(getenv(“CLEARDB_DATABASE_URL”));
 return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => '127.0.0.1',
-
-	// The name of the database to select.
-	'database' => 'para',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => '',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
+‘server’ => $url[“host”],
+‘user’ => $url[“root”],
+‘password’ => $url[“”],
+‘database’ => substr($url[“path”],1),
+‘tablePrefix’ => ‘craft’,
 );
