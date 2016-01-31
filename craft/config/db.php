fde76ced8cb8511c1e7af630373c53b4660390bf
the incredible hulch <jhulcher@gmail.com>
2016-01-31 11:12:49 -0800

    Change .htaccess

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,33 +7,33 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 //
-// return array(
-//
-// 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-// 	'server' => '127.0.0.1',
-//
-// 	// The name of the database to select.
-// 	'database' => 'para',
-//
-// 	// The database username to connect with.
-// 	'user' => 'root',
-//
-// 	// The database password to connect with.
-// 	'password' => '',
-//
-// 	// The prefix to use when naming tables. This can be no more than 5 characters.
-// 	'tablePrefix' => 'craft',
-//
-// );
-
-$url=parse_url(getenv(“CLEARDB_DATABASE_URL”));
-
 return array(
 
-‘server’ => "172.11.56.78",
-‘database’ "heroku_73495891f3b4fc1",
-‘user’ => "ba6e2134280371",
-‘password’ => "766df6b0",
-‘tablePrefix’ => ‘craft’,
+	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+	'server' => '127.0.0.1',
+
+	// The name of the database to select.
+	'database' => 'para',
+
+	// The database username to connect with.
+	'user' => 'root',
+
+	// The database password to connect with.
+	'password' => '',
+
+	// The prefix to use when naming tables. This can be no more than 5 characters.
+	'tablePrefix' => 'craft',
 
 );
+
+// $url=parse_url(getenv(“CLEARDB_DATABASE_URL”));
+//
+// return array(
+//
+// ‘server’ => $url[“host”],
+// ‘database’ => substr($url[“path”],1),
+// ‘user’ => $url[“user”],
+// ‘password’ => $url[“pass”],
+// ‘tablePrefix’ => ‘craft’,
+//
+// );
