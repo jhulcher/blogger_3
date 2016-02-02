814780701eeb9f6141994400a4525f87e8529e81
the incredible hulch <jhulcher@gmail.com>
2016-02-02 14:33:59 -0800

    Remove dev css

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,36 +7,36 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 //
-return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => '127.0.0.1',
-
-	// The name of the database to select.
-	'database' => 'para',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => '',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
-);
-
-//
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // return array(
+//
 // 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-// 	'server' => $url["host"],
-// 	// The database username to connect with.
-// 	'user' => $url["user"],
-// 	// The database password to connect with.
-// 	'password' => $url["pass"],
+// 	'server' => '127.0.0.1',
+//
 // 	// The name of the database to select.
-// 	'database' => substr($url["path"],1),
+// 	'database' => 'para',
+//
+// 	// The database username to connect with.
+// 	'user' => 'root',
+//
+// 	// The database password to connect with.
+// 	'password' => '',
+//
 // 	// The prefix to use when naming tables. This can be no more than 5 characters.
 // 	'tablePrefix' => 'craft',
+//
 // );
+
+//
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+return array(
+	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+	'server' => $url["host"],
+	// The database username to connect with.
+	'user' => $url["user"],
+	// The database password to connect with.
+	'password' => $url["pass"],
+	// The name of the database to select.
+	'database' => substr($url["path"],1),
+	// The prefix to use when naming tables. This can be no more than 5 characters.
+	'tablePrefix' => 'craft',
+);
